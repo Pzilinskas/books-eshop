545a106b6651aab0adca4c0d8553e37b8e2b9c74
Paulius <zilinskas.paulius@gmail.com>
2020-03-07 16:33:50 +0200

    initial layout and folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-const App = () => <div>Ebooks Shop Application </div>;
 
-export default App;
+import AppMenu from './components/menu/appMenu'
+import HomePage from "./pages/homePage";
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    }
+}));
+
+export default function CenteredGrid() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+                <AppMenu />
+                <Router>
+                    <Route path="/" exact component={HomePage} />
+                </Router>
+        </div>
+    );
+}
---
+++ src/pages/homePage.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+import Book from '../components/book/book'
+
+
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
+
+export default function HomePage() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <Grid container spacing={2}>
+                <Grid item xs={12} sm={6} md={4}>
+                <Book />
+                </Grid>
+                <Grid item xs={12} sm={6} md={4}>
+                <Book />
+                </Grid>
+            </Grid>
+        </div>
+    );
+}
